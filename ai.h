3847aa491601bb5a6123ef439fba4e198317c232
abulkalamazad1425 <bsse1425@iit.du.ac.bd>
2025-05-18 10:04:30 +0600

    ai.h

---
+++ ai.h
@@ -0,0 +1,12 @@
+#ifndef AI_H
+#define AI_H
+
+#include "game.h"
+
+#define MAX_DEPTH 3
+
+int evaluateBoard(GameState *game);
+int minimax(GameState *game, int depth, int alpha, int beta, bool maximizingPlayer);
+int findBestMove(GameState *game);
+
+#endif
